724096f3c7cb71853956aeb17a892034bbfa331c
Delvin <143737853+bancakes@users.noreply.github.com>
2024-12-12 04:11:34 +0000

    changes

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
+from .forms import BlogPostForm
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+# listview to get all blog posts 
+def post_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+# detail
+def post_detail(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    return render(request, 'blog/post_detail.html', {'post' : post})
+# create a new post 
+def post_create(request):
+    if request.method == 'POST':  
+        form = BlogPostForm(request.POST) 
+        if form.is_valid(): 
+            form.save() 
+            return redirect('post_list') 
+    else:
+        form = BlogPostForm() 
+
+    return render(request, 'blog/post_form.html', {'form': form})  
+# View/edit existing blog post
+def post_edit(request, id):
+    post = get_object_or_404(BlogPost, id=id)  
+    if request.method == 'POST':  
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():  
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = BlogPostForm(instance=post)
+
+    return render(request, 'blog/post_form.html', {'form': form, 'post': post})
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-+iv*$+b9e3p1@*)^+g!j(h6ko-%g!65z1uot0^ur0g&mxwexta
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,11 +114,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
